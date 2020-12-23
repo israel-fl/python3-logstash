d06ee029edf298df50b7346bd17aabb860d4ac7e
Myrik <myrik260138@tut.by>
2020-12-23 18:50:39 +0100

    update simple fields processor

--- logstash/formatter.py
+++ logstash/formatter.py
@@ -13,15 +13,15 @@ class LogstashFormatterBase(logging.Formatter):
     # The list contains all the attributes listed in
     # http://docs.python.org/library/logging.html#logrecord-attributes
     skip_list = (
-        'args', 'asctime', 'created', 'exc_info', 'exc_text', 'filename',
-        'funcName', 'id', 'levelname', 'levelno', 'lineno', 'module',
-        'msecs', 'msecs', 'message', 'msg', 'name', 'pathname', 'process',
-        'processName', 'relativeCreated', 'thread', 'threadName', 'extra'
+        "args", "asctime", "created", "exc_info", "exc_text", "filename",
+        "funcName", "id", "levelname", "levelno", "lineno", "module",
+        "msecs", "msecs", "message", "msg", "name", "pathname", "process",
+        "processName", "relativeCreated", "thread", "threadName", "extra"
     )
 
-    easy_types = (str, bool, dict, float, int, list, type(None))
+    easy_types = (str, bool, float, int, type(None))
 
-    def __init__(self, message_type='Logstash', tags=None, fqdn=False):
+    def __init__(self, message_type="Logstash", tags=None, fqdn=False):
         self.message_type = message_type
         self.tags = tags if tags is not None else []
 
@@ -30,33 +30,40 @@ class LogstashFormatterBase(logging.Formatter):
         else:
             self.host = socket.gethostname()
 
+    def simplify(self, value):
+        if isinstance(value, self.easy_types):
+            return value
+        elif isinstance(value, (list, tuple, set)):
+            return type(value)(self.simplify(i) for i in value)
+        elif isinstance(value, dict):
+            return {self.simplify(k): self.simplify(v) for k, v in value.items()}
+        else:
+            return repr(value)
+
     def get_extra_fields(self, record):
         fields = {}
 
         for key, value in record.__dict__.items():
             if key not in self.skip_list:
-                if isinstance(value, self.easy_types):
-                    fields[key] = value
-                else:
-                    fields[key] = repr(value)
+                fields[key] = self.simplify(value)
 
         return fields
 
     def get_debug_fields(self, record):
         fields = {
-            'stack_trace': self.format_exception(record.exc_info),
-            'lineno': record.lineno,
-            'process': record.process,
-            'thread_name': record.threadName,
+            "stack_trace": self.format_exception(record.exc_info),
+            "lineno": record.lineno,
+            "process": record.process,
+            "thread_name": record.threadName,
         }
 
         # funcName was added in 2.5
-        if not getattr(record, 'funcName', None):
-            fields['funcName'] = record.funcName
+        if not getattr(record, "funcName", None):
+            fields["funcName"] = record.funcName
 
         # processName was added in 2.6
-        if not getattr(record, 'processName', None):
-            fields['processName'] = record.processName
+        if not getattr(record, "processName", None):
+            fields["processName"] = record.processName
 
         return fields
 
@@ -67,11 +74,14 @@ class LogstashFormatterBase(logging.Formatter):
     @classmethod
     def format_timestamp(cls, time):
         tstamp = datetime.utcfromtimestamp(time)
-        return tstamp.strftime("%Y-%m-%dT%H:%M:%S") + ".%03dZ" % (tstamp.microsecond / 1000)
+        return "%s.%03dZ" % (
+            tstamp.strftime("%Y-%m-%dT%H:%M:%S"),
+            tstamp.microsecond / 1000,
+        )
 
     @classmethod
     def format_exception(cls, exc_info):
-        return ''.join(traceback.format_exception(*exc_info)) if exc_info else ''
+        return "".join(traceback.format_exception(*exc_info)) if exc_info else ""
 
     @classmethod
     def serialize(cls, message):
@@ -84,47 +94,45 @@ class LogstashFormatterVersion0(LogstashFormatterBase):
     def format(self, record):
         # Create message dict
         message = {
-            '@timestamp': self.format_timestamp(record.created),
-            '@message': record.getMessage(),
-            '@source': self.format_source(
+            "@timestamp": self.format_timestamp(record.created),
+            "@message": record.getMessage(),
+            "@source": self.format_source(
                 self.message_type, self.host, record.pathname
             ),
-            '@source_host': self.host,
-            '@source_path': record.pathname,
-            '@tags': self.tags,
-            '@type': self.message_type,
-            '@fields': {
-                'levelname': record.levelname,
-                'logger': record.name,
+            "@source_host": self.host,
+            "@source_path": record.pathname,
+            "@tags": self.tags,
+            "@type": self.message_type,
+            "@fields": {
+                "levelname": record.levelname,
+                "logger": record.name,
             },
         }
 
         # Add extra fields
-        message['@fields'].update(self.get_extra_fields(record))
+        message["@fields"].update(self.get_extra_fields(record))
 
         # If exception, add debug info
         if record.exc_info:
-            message['@fields'].update(self.get_debug_fields(record))
+            message["@fields"].update(self.get_debug_fields(record))
 
         return self.serialize(message)
 
 
 class LogstashFormatterVersion1(LogstashFormatterBase):
-
     def format(self, record):
         # Create message dict
         message = {
-            '@timestamp': self.format_timestamp(record.created),
-            '@version': '1',
-            'message': record.getMessage(),
-            'host': self.host,
-            'path': record.pathname,
-            'tags': self.tags,
-            'type': self.message_type,
-
+            "@timestamp": self.format_timestamp(record.created),
+            "@version": "1",
+            "message": record.getMessage(),
+            "host": self.host,
+            "path": record.pathname,
+            "tags": self.tags,
+            "type": self.message_type,
             # Extra Fields
-            'level': record.levelname,
-            'logger_name': record.name,
+            "level": record.levelname,
+            "logger_name": record.name,
         }
 
         # Add extra fields
